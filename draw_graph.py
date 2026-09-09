2952d89af5ca1cd2f69fe6d123dbfe0a93d5ed30
siddharth-mcs26-web <siddharth-mcs26@cs.du.ac.in>
2026-09-09 07:56:23 +0000

    done

--- draw_graph.py
+++ draw_graph.py
@@ -11,7 +11,8 @@ def draw_graph(edges, cover):
         else:
             colors.append('lightblue')
     nx.draw(G,node_color=colors, with_labels=True)
-    plt.show()
+    plt.savefig(f"graphpic{len(edges)}")
+    plt.close()
 
 draw_graph([(7, 6), (6, 3), (4, 7), (6, 2), (6, 9), (5, 0), (7, 8), (4, 5), (0, 8), (6, 5), (7, 9), (4, 9), (2, 3), (3, 1), (2, 4), (2, 8), (9, 8), (9, 0), (0, 4), (8, 5), (2, 9), (1, 0), (1, 6), (4, 6), (9, 3), (6, 8), (3, 8), (2, 7), (5, 3), (2, 0), (5, 1), (7, 3), (1, 2), (1, 9)], [5,6])
     
--- main.py
+++ main.py
@@ -2,26 +2,56 @@ from graph_generator import generate_graph
 from brute_force_vertex_cover import brute_force_vertex_cover
 from read_output import read_file
 from draw_graph import draw_graph
+
 import time
+import csv
+
 n = 10
+
 results = []
+
 for m in range(15, 46, 5):
-    generate_graph(n,m)
+
+    generate_graph(n, m)
+
     graph = read_file(f"graph{m}.txt")
+
     start = time.perf_counter()
+
     cover = brute_force_vertex_cover(graph)
-    print(cover)
+
     end = time.perf_counter()
+
     results.append({
-        "n" : n,
-        "m" : m,
-        "vc_size" : len(cover),
-        "time" : end-start
+        "n": n,
+        "m": m,
+        "vc_size": len(cover),
+        "time": end - start
     })
+
+    print(cover)
+
     draw_graph(graph, cover)
+
+
+# Print results
 for r in results:
     print(
         f"({r['n']},{r['m']})\t\t"
         f"{r['vc_size']}\t"
         f"{r['time']:.6f} s"
     )
+
+
+# Save results to CSV
+with open("vertex_cover_timing.csv", "w", newline="") as file:
+
+    writer = csv.DictWriter(
+        file,
+        fieldnames=["n", "m", "vc_size", "time"]
+    )
+
+    writer.writeheader()
+    writer.writerows(results)
+
+print("\nResults saved to vertex_cover_timing.csv")
